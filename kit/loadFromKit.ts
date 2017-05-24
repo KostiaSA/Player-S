5a3d44b86fe9e5967442b0052835b0a89b2696ec
KostiaSA <sonyk795>
2017-05-24 14:14:26 +0300

    --

--- kit/loadFromKit.ts
+++ kit/loadFromKit.ts
@@ -2,7 +2,6 @@ import {dateTimeAsSql, stringAsSql} from "../sql/SqlCore";
 import {executeSql} from "../sql/MsSqlDb";
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database("C:/--EdemPlayer--/S/static/KIT/epg.db");
-//var db = new sqlite3.Database("c:/KIT-YANDEX/epg.db");
 
 export async function loadChannels(): Promise<void> {
 
